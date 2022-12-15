a31ced6e3deec104017255801821f8816f803396
goo951019 <goo1140@gmail.com>
2022-12-15 11:15:52 -0500

    second commit

--- src/pages/Page_Landing.js
+++ src/pages/Page_Landing.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Page_Landing() {
     return (
-        <div className="container" style={{background: 'black', width: '100wh'}}>
-            <h1>hi</h1>
+        <div className="container" style={{width: '100wh'}}>
+            <h1>BCS V-2.0.0</h1>
+            <button>hello</button>
         </div>
     )
 }
